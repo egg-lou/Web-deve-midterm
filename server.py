8652f18270d4895e82a2c02fd40542e97eb1e351
egg-lou <miguel.rafaellouie2003@gmail.com>
2024-12-09 14:55:42 +0800

    updated port

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import sys
+import os
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
-port = 8000
-print(f"Starting server at http://localhost:{port}")
-httpd = HTTPServer(('localhost', port), CORSRequestHandler)
+port = int(os.environ.get('PORT', 8000))
+print(f"Starting server on port {port}")
+httpd = HTTPServer(('0.0.0.0', port), CORSRequestHandler)
 httpd.serve_forever()
